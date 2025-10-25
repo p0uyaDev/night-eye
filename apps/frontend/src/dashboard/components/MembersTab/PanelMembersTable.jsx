026cf0cf894952ff71bcce974686dacb519983ef
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-10-26 01:55:37 +0330

    feat(panel-members): add conditon to member table based on roles and current user id

--- apps/frontend/src/dashboard/components/MembersTab/PanelMembersTable.jsx
+++ apps/frontend/src/dashboard/components/MembersTab/PanelMembersTable.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
 import MembersTableBody from "./table/layouts/MembersTableBody";
+import { AuthContext } from "../../../shared/context/AuthContext";
 import { users } from "../../../shared/dummy";
 
 export default function PanelMembersTable() {
+  const { id: currentUserId, isAdmin, IsOwner } = useContext(AuthContext);
+
+  let filteredMembers = users.filter(
+    (u) => u.id !== currentUserId && u.role !== "owner",
+  );
+
+  if (!isAdmin && !IsOwner) {
+    filteredMembers = filteredMembers.filter((u) => u.role !== "writer");
+  }
+
   return (
     <table className="table w-3/5">
-      {users.map((u) => {
+      {filteredMembers.map((u) => {
         return <MembersTableBody key={u.id} membersItem={u} />;
       })}
     </table>
--- apps/frontend/src/dashboard/components/MembersTab/table/util/MembersTableActions.jsx
+++ apps/frontend/src/dashboard/components/MembersTab/table/util/MembersTableActions.jsx
@@ -1,5 +1,7 @@
+import { useContext } from "react";
 import { SquarePen, Delete } from "lucide-react";
 import Actions from "../../../../../shared/components/UIElements/Actions";
+import { AuthContext } from "../../../../../shared/context/AuthContext";
 
 export default function MembersTableActions({
   membersItem,
@@ -8,6 +10,14 @@ export default function MembersTableActions({
   actions = ["edit", "delete"],
   mode = "row",
 }) {
+  const { isAdmin, isOwner, id: currentUserId } = useContext(AuthContext);
+  const canModify =
+    (isOwner && membersItem.id !== currentUserId) ||
+    (isAdmin &&
+      !isOwner &&
+      membersItem.role === "writer" &&
+      membersItem.id !== currentUserId);
+
   const actionMap = {
     edit: {
       label: "Edit",
@@ -27,5 +37,9 @@ export default function MembersTableActions({
     .map((action) => actionMap[action])
     .filter(Boolean);
 
-  return <Actions items={membersItem} actions={actionItems} mode={mode} />;
+  return (
+    <div disabled={!canModify}>
+      <Actions items={membersItem} actions={actionItems} mode={mode} />
+    </div>
+  );
 }
