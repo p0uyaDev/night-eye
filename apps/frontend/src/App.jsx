649726aadb01fe59fa9cd8ad86276390a4714207
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-11-11 02:04:57 +0330

    feat(panel-settings): add under maintenance context

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import ScrollToTop from "./shared/util/ScrollToTop";
 import { AlertProvider } from "./shared/context/AlertContext";
+import { AuthContext } from "./shared/context/AuthContext";
 import { AuthProvider } from "./shared/context/AuthProvider";
+import UnderMaintenance from "./pages/UnderMaintenance";
+import { SettingsContext } from "./shared/context/SettingsContext";
+
 import "./App.css";
 
 function App() {
@@ -11,14 +16,25 @@ function App() {
       <AuthProvider>
         <AlertProvider>
           <ScrollToTop />
-          <Routes>
-            {routes.map(({ path, element }, index) => (
-              <Route key={index} path={path} element={element} />
-            ))}
-          </Routes>
+          <AppRoutes />
         </AlertProvider>
       </AuthProvider>
     </Router>
+  );
+}
+
+function AppRoutes() {
+  const { isMaintenance } = useContext(SettingsContext);
+  const { isOwner, isAdmin } = useContext(AuthContext);
+
+  return isMaintenance && !isOwner && !isAdmin ? (
+    <UnderMaintenance />
+  ) : (
+    <Routes>
+      {routes.map(({ path, element }, index) => (
+        <Route key={index} path={path} element={element} />
+      ))}
+    </Routes>
   );
 }
 
