62e9d207aa4e2b248670a0ce7e5af9188cbac5e3
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-11-11 00:08:18 +0330

    feat(panel-nav): add condition to navbar based on isLoggedIn in AuthContext

--- apps/frontend/src/shared/Layouts/Header.jsx
+++ apps/frontend/src/shared/Layouts/Header.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import MainNav from "../components/Navigation/MainNav";
 import PanelNav from "../components/Navigation/PanelNav";
 import { mainNavLinks } from "../../routes";
 
 function Header({ type = "main" }) {
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
     <header>
-      {type === "main" ? (
+      {type === "main" && !isLoggedIn ? (
         <MainNav links={mainNavLinks} />
       ) : (
         <PanelNav links={mainNavLinks} />
