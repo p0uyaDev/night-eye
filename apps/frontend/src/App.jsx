1092ba63c6b9bef2d75f6f1c3ffa67cf5d76a232
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-11-11 19:47:05 +0330

    improvement(app): move app route to separate component

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import { useContext } from "react";
-import { Helmet } from "react-helmet-async";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import routes from "./routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 import ScrollToTop from "./shared/util/ScrollToTop";
 import { AlertProvider } from "./shared/context/AlertContext";
-import { AuthContext } from "./shared/context/AuthContext";
 import { AuthProvider } from "./shared/context/AuthProvider";
-import UnderMaintenance from "./pages/UnderMaintenance";
-import { SettingsContext } from "./shared/context/SettingsContext";
 import SettingsProvider from "./shared/context/SettingsProvider";
 
 import "./App.css";
@@ -24,28 +19,6 @@ function App() {
         </SettingsProvider>
       </AuthProvider>
     </Router>
-  );
-}
-
-function AppRoutes() {
-  const { isMaintenance, isRobotsBlocked } = useContext(SettingsContext);
-  const { isOwner, isAdmin } = useContext(AuthContext);
-
-  return (
-    <>
-      <Helmet>
-        {isRobotsBlocked && <meta name="robots" content="noindex, nofollow" />}
-      </Helmet>
-      {isMaintenance && !isOwner && !isAdmin ? (
-        <UnderMaintenance />
-      ) : (
-        <Routes>
-          {routes.map(({ path, element }, index) => (
-            <Route key={index} path={path} element={element} />
-          ))}
-        </Routes>
-      )}
-    </>
   );
 }
 
