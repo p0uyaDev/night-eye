4ce5e19b1745d022d48236f1add02549dd6500e3
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-11-16 02:19:02 +0330

    fix(News): fix news pagination and unlmited loop

--- apps/frontend/src/news/components/NewsPagination.jsx
+++ apps/frontend/src/news/components/NewsPagination.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Pagination from "../../shared/components/UIElements/Pagination/Pagination";
 
 const PAGE_SIZE = 3; //TODO: move to .env or at least put in a config file
@@ -6,20 +6,18 @@ const PAGE_SIZE = 3; //TODO: move to .env or at least put in a config file
 function NewsPagination({ news, onPageChange }) {
   const [currentPage, setCurrentPage] = useState(1);
   const totalPage = Math.ceil(news.length / PAGE_SIZE);
-
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [news]);
+  const previousSliceRef = useRef([]);
 
   useEffect(() => {
     const start = (currentPage - 1) * PAGE_SIZE;
     const end = start + PAGE_SIZE;
     const newSlice = news.slice(start, end);
 
-    onPageChange((prev) => {
-      if (JSON.stringify(prev) === JSON.stringify(newSlice)) return prev;
-      return newSlice;
-    });
+    const prev = previousSliceRef.current;
+    if (JSON.stringify(prev) !== JSON.stringify(newSlice)) {
+      previousSliceRef.current = newSlice;
+      onPageChange(newSlice);
+    }
   }, [currentPage, news, onPageChange]);
 
   function handlePageChange(page) {
